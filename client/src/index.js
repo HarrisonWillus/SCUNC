a69318ee7da204917bd435d5bd48b7d21e8ba7c6
HarrisonWillus <hwilliams1867@gmail.com>
2025-09-05 12:54:59 -0400

    UPDATE: added speedInsights to the website for vercel

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,13 @@
 import ReactDOM from 'react-dom/client';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <>
+        <App />
+        <SpeedInsights />
+    </>
+    
 );
