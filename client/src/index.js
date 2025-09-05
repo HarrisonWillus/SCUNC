f428eaa25bbb6ee98568e5f80c91089ba5f31343
HarrisonWillus <hwilliams1867@gmail.com>
2025-09-05 14:07:43 -0400

    FIX: the dependencies are now up to date and everything should be working properly now added SEO too

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,8 @@
 import ReactDOM from 'react-dom/client';
-import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HelmetProvider>
     <App />
-  </HelmetProvider>
 );
